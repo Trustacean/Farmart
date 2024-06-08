f2293fe757265d605285b8b0bb487f3dcc3ad790
Trustacean <pizzacrustacean15@gmail.com>
2024-06-08 15:44:36 +0700

    feat: added login page
    added login page and deleted default welcome page that comes with laravel framework

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->user_id = uniqid();
+        $user->user_phone = $request->user_phone;
+        $user->user_password = Hash::make($request->user_password);
+        $user->user_name = $request->user_name;
+        $user->user_postal_code = 0;
+        $user->save();
+        return redirect('/login');
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('user_phone', $request->user_phone)->first();
+        if (!$user || !Hash::check($request->user_password, $user->user_password)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        return response()->json([
+            'message' => 'Authorized'
+        ], 200);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,29 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Ramsey\Uuid\Uuid;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'users';
+    protected $primaryKey = 'user_id';
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+        'user_phone',
+        'user_password',
+        'user_name',
+        'user_postal_code'];
+    protected $keyType = 'string';
+    public $timestamps = false;
+    public $incrementing = false;
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected static function boot()
+    {
+        parent::boot();
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Uuid::uuid4()->toString();
+        });
+    }
 }
