598bd8be604d4adfebc9c8a501aa2e87ded70380
Trustacean <pizzacrustacean15@gmail.com>
2024-06-09 21:04:07 +0700

    fix: moved view returning controls from routes to controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,75 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\User;
 
 class UserController extends Controller
 {
+
+    public function showOnboardPage()
+    {
+        if (session()->has('user_id')) {
+            return redirect('/home');
+        }
+        return view('onboard');
+    }
+
+    public function showRegisterPage()
+    {
+        return view('register');
+    }
+
+    public function showLoginPage()
+    {
+        if (session()->has('user_id')) {
+            return redirect('/home');
+        }
+        return view('login');
+    }
+
+    public function showHomePage()
+    {
+        if (!session()->has('user_id')) {
+            return view('home');
+        }
+        $user = User::where('user_id', session('user_id'))->first();
+        return view('home', ['user' => $user]);
+    }
+
+    public function showProfilePage()
+    {
+        if (!session()->has('user_id')) {
+            return redirect('/login');
+        }
+        $user = User::where('user_id', session('user_id'))->first();
+        return view('profile/profile', ['user' => $user]);
+    }
+
+    public function showSellerRegisterPage()
+    {
+        if (!session()->has('user_id')) {
+            return redirect('/login');
+        }
+        return view('seller/register');
+    }
+
+    public function showEditAddressPage()
+    {
+        if (!session()->has('user_id')) {
+            return redirect('/login');
+        }
+        $user = User::where('user_id', session('user_id'))->first();
+        return view('profile/address', ['user' => $user]);
+    }
+
+    public function logout()
+    {
+        session()->forget('user_id');
+        return redirect('/login');
+    }
+
+
     public function store(Request $request)
     {
         $user = new User();
@@ -17,13 +80,13 @@ class UserController extends Controller
         $user->user_password = Hash::make($request->user_password);
         $user->user_name = $request->user_name;
         $user->user_postal_code = 0;
+        $user->user_address = '';
         $user->save();
         return redirect('/login');
     }
 
     public function login(Request $request)
     {
-        session()->forget('user_id');
         $user = User::where('user_phone', $request->user_phone)->first();
         if (!$user || !Hash::check($request->user_password, $user->user_password)) {
             return response()->json([
@@ -31,8 +94,7 @@ class UserController extends Controller
             ], 401);
         } else {
             session(['user_id' => $user->user_id]);
-            return view('home', ['user' => $user]);
+            return redirect('/home');
         }
-        
     }
 }
